b7e7122d07416699ce31f64186409312e35c755e
akmal <akmalns@gmail.com>
2023-08-21 07:45:47 +0000

    adjust mcs mapping, comment print

--- src/5gnrsch/sch.c
+++ src/5gnrsch/sch.c
@@ -2374,8 +2374,8 @@ uint8_t SchProcDlCqiInd(Pst *pst, SchDlCqiInd *dlCqiInd)
    SchUeCb *ueCb = NULLP;
    SchCellCb *cell = NULLP;
    Inst  inst = pst->dstInst-SCH_INST_START;
-   uint8_t mcsMappingTable[] = {1,1,1,2,2,3,3,4,4,5,5,6,6,8,8};
-   DU_LOG("\nINFO  -->  SCH : Received DL CQI Ind %d",dlCqiInd->dlCqiRpt.cqi);   
+   uint8_t mcsMappingTable[] = {0,0,1,3,5,7,9,11,13,15,17,19,21,23,25,27};
+   // DU_LOG("\nINFO  -->  SCH : Received DL CQI Ind Value %d, MCS Value %d",dlCqiInd->dlCqiRpt.cqi,mcsMappingTable[dlCqiInd->dlCqiRpt.cqi-1]);   
 
    if(!dlCqiInd)
    {
@@ -2405,9 +2405,10 @@ uint8_t SchProcDlCqiInd(Pst *pst, SchDlCqiInd *dlCqiInd)
             else
             {
                /*TODO: complete the processing of DL CQI Ind*/ 
-               DU_LOG("\nINFO   -->   SCH : Processing DL CQI Ind. CQI Value = %d, Updated MCS = %d",dlCqiInd->dlCqiRpt.cqi,mcsMappingTable[dlCqiInd->dlCqiRpt.cqi-1]);   
+               // DU_LOG("\nINFO   -->   SCH : Processing DL CQI Ind. CQI Value = %d, Updated MCS = %d",dlCqiInd->dlCqiRpt.cqi,mcsMappingTable[dlCqiInd->dlCqiRpt.cqi-1]);   
                //Updating MCS value
                ueCb->ueCfg.dlModInfo.mcsIndex = mcsMappingTable[dlCqiInd->dlCqiRpt.cqi-1];
+               // ueCb->ueCfg.dlModInfo.mcsIndex = 4;
             }
          }
          else
